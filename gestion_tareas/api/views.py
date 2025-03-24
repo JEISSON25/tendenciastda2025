4d73bd245a730c6bcc0d1e4597f5ce9129e4092b
Jonathanzh16 <jonathanzh16@gmail.com>
2025-03-24 15:01:40 -0500

    AggViews

--- gestion_tareas/api/views.py
+++ gestion_tareas/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from .models import Proyecto, Tarea
+from .serializers import ProyectoSerializer, TareaSerializer
+from rest_framework.permissions import IsAuthenticated
+ 
+class ProyectoViewSet(viewsets.ModelViewSet):
+    queryset = Proyecto.objects.all()
+    serializer_class = ProyectoSerializer
+    permission_classes = [IsAuthenticated]
+ 
+class TareaViewSet(viewsets.ModelViewSet):
+    queryset = Tarea.objects.all()
+    serializer_class = TareaSerializer
+    permission_classes = [IsAuthenticated]
