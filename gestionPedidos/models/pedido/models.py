5ff9cd55d089dff1b51ebda068312e753b2dde5e
ANAISA123 <ana.agudelo95@correo.tdea.edu.co>
2025-03-27 16:06:15 -0500

    The order registration view has been improved

--- gestionPedidos/models/pedido/models.py
+++ gestionPedidos/models/pedido/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class  Pedido (models.Model):
     fecha_creacion = models.DateTimeField("Fecha", auto_now_add=True)
     direccion = models.CharField("Direccion", max_length=255)
-    descripcion= models.CharField('Descripcion', max_length=300)
+    descripcion= models.TextField('Descripcion', max_length=300)
     precio_total = models.DecimalField("Total", max_digits=20, decimal_places=2)
-    estado = models.CharField("Estado Actual", max_length=20, default='pendiente')
+    estado = models.CharField("Estado Actual", max_length=20)
 
     def __str__(self):
         return f'{self.fecha_creacion} - {self.direccion} -  {self.descripcion} - {self.precio_total} - {self.estado}'
